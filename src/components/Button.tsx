fba34f67fed3f48bbb16c93a924feaa59f3713c0
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-06-06 23:04:03 +0900

    Storybook周りの調整

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,8 +13,8 @@ const Button: React.FC<React.ComponentProps<typeof HuiButton>> = ({
       'border border-1 border-slate-500 rounded-md',
       'dark:border-slate-300',
       'text-slate-500 dark:text-slate-100 p-1',
-      'hover:bg-slate-200',
-      'focus:outline focus:outline-2 focus:outline-slate-400',
+      'hover:bg-slate-200 dark:hover:bg-slate-100/10',
+      'active:outline active:outline-1 active:outline-slate-400 dark:active:outline-slate-200',
       className,
     )}
   >
