1df74719a06eb0dd9b6a6c4d0c141e282840387f
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-06-08 02:19:25 +0900

    drizzleマイグレーション用設定ファイルの追加

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'mysql',
+  dbCredentials: {
+    host: process.env.DB_HOST!,
+    user: process.env.MYSQL_USER!,
+    password: process.env.MYSQL_PASSWORD!,
+    database: process.env.MYSQL_DATABASE!,
+  }
+});
+
