3b8d8d61d95ba71884d3dab04ba21e9434ce6ca3
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-06-28 19:01:47 +0900

    matcherの調整中...
    画像ファイルがルートURLにリダイレクトされてしまっている...なぜ？

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,9 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  //matcher: ['/', '/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/', '/((?!api|_next/static|_next/image|public).*)'],
+  //matcher: ['/', '/((?!api|_next/static|_next/image|.*\\.(png|jpg|jpeg|gif|svg|ico|webp|avif|bmp|tiff|tif)$).*)'],
   //matcher: '/*',
 };
 
