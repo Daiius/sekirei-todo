e72fbccf7b75045e1db8af0d724ec38ddfba0eb1
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-08-02 02:37:28 +0900

    GithubのユーザIDを取得できるように修正

--- src/auth.ts
+++ src/auth.ts
@@ -3,5 +3,40 @@ import GitHub from "next-auth/providers/github"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        token.sub = profile.id ?? '';
+        // emailは空になっている、取得するには
+        // GithubのOAuth設定をきちんと変更する必要がありそう
+        // (emailを教えてよいとは設定していないと思うので...)
+        token.email = profile.email;
+        token.username = profile.login;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.sub ?? '';
+      session.user.email = token.email ?? '';
+      //session.user.username = token.username ?? '';
+      return session;
+    },
+    async authorized({ auth, request: { nextUrl }}) {
+      console.log('authorized: ', auth);
+      const isLoggedIn = !!auth?.user;
+      const isOnRoot = nextUrl.pathname.startsWith('/sekirei-todo');
+      if (isOnRoot) {
+        // 未ログインならログインページへ
+        // ...trueならそのまま通して、
+        // falseならログインページを表示する感じ？
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        // ログイン済みなのにルートページにいない場合には
+        // リダイレクトする
+        return Response.redirect(new URL('/sekirei-todo', nextUrl));
+      }
+      return true;
+    },
+  }
 });
 
