fcdc7770ec8da4e57b44b21f10b53d1fc3c44dcc
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-08-30 16:51:11 +0900

    データベース管理を簡易化
    drizzle-kit pushによるテストデータ生成
    プロジェクト構成を単純化

---
+++ addTestData.ts
@@ -0,0 +1,22 @@
+import { tasks } from '@/db/schema';
+
+import { drizzle } from 'drizzle-orm/mysql2';
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+
+const db = drizzle(connection);
+
+await db.insert(tasks).values([{
+  userId: '5844472',
+  description: 'this is a test task!',
+}]);
+
+await connection.end();
+
--- migrate.ts
+++
@@ -1,7 +0,0 @@
-import  { migrate } from 'drizzle-orm/mysql2/migrator';
-import { db, connection } from '@/db';
-
-await migrate(db, { migrationsFolder: './.drizzle' });
-
-await connection.end();
-
