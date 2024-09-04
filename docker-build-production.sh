b3a6a8a25a798e6e0b76ec1631e78370edbae531
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-05 03:00:06 +0900

    ビルド時にDBアクセスが不要なので簡略化

---
+++ docker-build-production.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+#echo 
+#echo Make sure port fowarding PRODUCTION database into localhost...
+#echo
+
+# 読み込む.envファイルを指定
+# #から始まるコメント行は無視
+# 行中に #から始まるコメントがあればそれも無視
+export $(cat .env.production .env | grep -v '^#' | sed 's/#.*//' | xargs)
+docker build \
+  --network host \
+  -t sekirei-todo-nextjs \
+  -f Dockerfile.nextjs.prod .
+
+echo Build done!
+
+exit $result
+
