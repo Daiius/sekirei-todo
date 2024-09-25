71dd7b36d0eef1c17285152c1f09ba2a108cf63f
Daiji Yamashita <yamashita.daiji@gmail.com>
2024-09-25 14:29:40 +0900

    改行位置が微妙だったのでレイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ export default function Page() {
   return (
     <div className='self-center flex flex-col'>
       <SekireiIcon className='self-center my-2'/>
-      <div className='self-center flex flex-col items-center'>
+      <div className='self-center items-center text-pretty mx-5'>
         <span> Sekirei Todoは、シンプルなTodoアプリです </span>
         <span> Githubアカウントで認証を行って使用できます </span>
         <div className='my-2'/>
-        <span>Sekirei Todo is a simple to-do list with a cute tail-swagger.</span>
-        <span>You can login to manage tasks by your Github account!</span>
+        <div>Sekirei Todo is a simple to-do list with a cute tail-swagger.</div>
+        <div>You can login to manage tasks by your Github account!</div>
       </div>
       <SignInButton className='self-center' />
     </div>
