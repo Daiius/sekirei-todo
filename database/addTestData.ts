b7542c30e0aa9fc9d6dd01e4a64571a679681374
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-11-01 17:07:44 +0900

    monorepo構成に合わせてデータベース関連ワークスペースをを参照

--- database/addTestData.ts
+++ database/addTestData.ts
@@ -1,13 +1,13 @@
-import { tasks } from '@/db/schema';
+import { tasks } from './db/schema';
 
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.DB_HOST!,
+  user: process.env.MYSQL_USER!,
+  password: process.env.MYSQL_PASSWORD!,
+  database: process.env.MYSQL_DATABASE!,
 });
 
 
