310efc57e002a73d4642f9b2d2c0c5c0b95cca9b
Daiji Yamashita <yamashita.daiji@gmail.com>
2025-11-03 17:13:23 +0900

    スケルトンを追加

--- nextjs/src/app/tasks/page.tsx
+++ nextjs/src/app/tasks/page.tsx
@@ -3,15 +3,23 @@ import { SekireiIcon } from '@/components/SekireiIcon';
 import { TaskInput } from '@/components/TaskInput';
 import { Suspense } from 'react';
 
+const TaskListSkeleton = () => (
+  <div className='flex flex-col gap-3 items-center'>
+    {Array.from({ length: 3 }).map((_, i) => 
+      <div key={i} className='w-full h-12 rounded-md bg-slate-300 dark:bg-slate-700 animate-pulse'/>
+    )}
+  </div>
+)
+
 export default function TasksPage() {
   return (
     <main className='w-full flex-1 flex flex-col items-center p-6 gap-6 md:p-24 overflow-y-auto'>
       <SekireiIcon />
       <div className='flex flex-col gap-6 w-full md:w-[60%]'>
-        <Suspense>
+        <Suspense fallback={<TaskListSkeleton />} >
           <TaskList />
         </Suspense>
-        <hr className='border-slate-500'/>
+        <hr className='border-slate-200 dark:border-slate-500'/>
         <TaskInput />
       </div>
     </main>
